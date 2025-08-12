78f22cecfa4e77a147bf12133962d43d619acf45
Shawon Islam <shawon250369@gmail.com>
2025-08-12 20:37:00 +0600

    Add files via upload

---
+++ pkg.sh
@@ -0,0 +1,23 @@
+clear
+echo "
+▗▄▄▖  ▗▄▄▖ ▗▄▄▖
+▐▌ ▐▌▐▌   ▐▌   
+▐▛▀▚▖▐▌    ▝▀▚▖
+▐▙▄▞▘▝▚▄▄▖▗▄▄▞▘
+" | lolcat
+echo "=============================================" | lolcat
+echo " Auture: Shawon Islam" | lolcat
+echo " Team Name:BCS TEAM" |lolcat
+echo " Tools: Basic Pakage Install" | lolcat
+echo "===============================================" | lolcat
+termux-setup-storage -y
+echo "Storage Permison Granted" | lolcat
+pkg update
+echo "Pkg install Succsfull" | lolcat 
+pkg install python
+echo "install Succsfull" | lolcat
+pkh install git
+echo " Succsfull"
+
+
+echo " Termux Setup Succsfull" | lolcat
